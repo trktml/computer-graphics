649ccd4732986209ac652aa8128e244eeed815e3
Tarık Temel <utariktemel@gmail.com>
2024-02-29 01:37:17 +0100

    initial commit

---
+++ CMakeFiles/engine.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/storage/school/Computer Graphics/02 - Opdracht 2D L-Systemen/code/engine.cc" "CMakeFiles/engine.dir/engine.cc.o" "gcc" "CMakeFiles/engine.dir/engine.cc.o.d"
+  "/mnt/c/storage/school/Computer Graphics/02 - Opdracht 2D L-Systemen/code/utils/easy_image.cc" "CMakeFiles/engine.dir/utils/easy_image.cc.o" "gcc" "CMakeFiles/engine.dir/utils/easy_image.cc.o.d"
+  "/mnt/c/storage/school/Computer Graphics/02 - Opdracht 2D L-Systemen/code/utils/ini_configuration.cc" "CMakeFiles/engine.dir/utils/ini_configuration.cc.o" "gcc" "CMakeFiles/engine.dir/utils/ini_configuration.cc.o.d"
+  "/mnt/c/storage/school/Computer Graphics/02 - Opdracht 2D L-Systemen/code/utils/l_parser.cc" "CMakeFiles/engine.dir/utils/l_parser.cc.o" "gcc" "CMakeFiles/engine.dir/utils/l_parser.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/engine.cc.o"
+  "CMakeFiles/engine.dir/engine.cc.o.d"
+  "CMakeFiles/engine.dir/utils/easy_image.cc.o"
+  "CMakeFiles/engine.dir/utils/easy_image.cc.o.d"
+  "CMakeFiles/engine.dir/utils/ini_configuration.cc.o"
+  "CMakeFiles/engine.dir/utils/ini_configuration.cc.o.d"
+  "CMakeFiles/engine.dir/utils/l_parser.cc.o"
+  "CMakeFiles/engine.dir/utils/l_parser.cc.o.d"
+  "engine"
+  "engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
