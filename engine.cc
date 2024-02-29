881a8105f3c9bcff8f1b67e904f7e002a07391e4
Tarık Temel <utariktemel@gmail.com>
2024-02-29 06:52:46 +0100

    cmake vscode debugging

--- engine.cc
+++ engine.cc
@@ -1,5 +1,8 @@
+
+
 #include "easy_image.h"
 #include "ini_configuration.h"
+#include "debug_tools.h"
 
 #include <fstream>
 #include <iostream>
@@ -8,37 +11,54 @@
 
 img::EasyImage generate_image(const ini::Configuration &configuration)
 {
-	return img::EasyImage();
+        int width = configuration["ImageProperties"]["width"].as_int_or_die();
+        int height = configuration["ImageProperties"]["height"].as_int_or_die();
+
+        img::EasyImage image(width, height);
+        for (unsigned int i = 0; i < width; i++)
+        {
+                for (unsigned int j = 0; j < height; j++)
+                {
+                        image(i, j) = img::Color(i % 256, j % 256, (i + j) % 256);
+                }
+        }
+
+        return image;
 }
 
-int main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
+        // for vscode debugging purposes, it gives "debug.ini" as first argument
+        const auto [argc, argv] = debug_tools::GET_DEBUG_ARGUMENTS(argc, argv);
+
         int retVal = 0;
         try
         {
-                std::vector<std::string> args = std::vector<std::string>(argv+1, argv+argc);
-                if (args.empty()) {
+                std::vector<std::string> args = std::vector<std::string>(argv + 1, argv + argc);
+                if (args.empty())
+                {
                         std::ifstream fileIn("filelist");
                         std::string filelistName;
-                        while (std::getline(fileIn, filelistName)) {
+                        while (std::getline(fileIn, filelistName))
+                        {
                                 args.push_back(filelistName);
                         }
                 }
-                for(std::string fileName : args)
+                for (std::string fileName : args)
                 {
                         ini::Configuration conf;
                         try
                         {
                                 std::ifstream fin(fileName);
-                                if (fin.peek() == std::istream::traits_type::eof()) {
-                                    std::cout << "Ini file appears empty. Does '" <<
-                                    fileName << "' exist?" << std::endl;
-                                    continue;
+                                if (fin.peek() == std::istream::traits_type::eof())
+                                {
+                                        std::cout << "Ini file appears empty. Does '" << fileName << "' exist?" << std::endl;
+                                        continue;
                                 }
                                 fin >> conf;
                                 fin.close();
                         }
-                        catch(ini::ParseException& ex)
+                        catch (ini::ParseException &ex)
                         {
                                 std::cerr << "Error parsing file: " << fileName << ": " << ex.what() << std::endl;
                                 retVal = 1;
@@ -48,25 +68,24 @@ int main(int argc, char const* argv[])
                         img::EasyImage image = generate_image(conf);
                         // log
                         std::cout << "Generated image of size " << image.get_height() << " x " << image.get_width() << std::endl;
-                        if(image.get_height() > 0 && image.get_width() > 0)
+                        if (image.get_height() > 0 && image.get_width() > 0)
                         {
                                 std::string::size_type pos = fileName.rfind('.');
-                                if(pos == std::string::npos)
+                                if (pos == std::string::npos)
                                 {
-                                        //filename does not contain a '.' --> append a '.bmp' suffix
+                                        // filename does not contain a '.' --> append a '.bmp' suffix
                                         fileName += ".bmp";
                                 }
                                 else
                                 {
-                                        fileName = fileName.substr(0,pos) + ".bmp";
+                                        fileName = fileName.substr(0, pos) + ".bmp";
                                 }
                                 try
                                 {
-                                        std::ofstream f_out(fileName.c_str(),std::ios::trunc | std::ios::out | std::ios::binary);
+                                        std::ofstream f_out(fileName.c_str(), std::ios::trunc | std::ios::out | std::ios::binary);
                                         f_out << image;
-
                                 }
-                                catch(std::exception& ex)
+                                catch (std::exception &ex)
                                 {
                                         std::cerr << "Failed to write image to file: " << ex.what() << std::endl;
                                         retVal = 1;
@@ -78,13 +97,13 @@ int main(int argc, char const* argv[])
                         }
                 }
         }
-        catch(const std::bad_alloc &exception)
+        catch (const std::bad_alloc &exception)
         {
-    		//When you run out of memory this exception is thrown. When this happens the return value of the program MUST be '100'.
-    		//Basically this return value tells our automated test scripts to run your engine on a pc with more memory.
-    		//(Unless of course you are already consuming the maximum allowed amount of memory)
-    		//If your engine does NOT adhere to this requirement you risk losing points because then our scripts will
-		//mark the test as failed while in reality it just needed a bit more memory
+                // When you run out of memory this exception is thrown. When this happens the return value of the program MUST be '100'.
+                // Basically this return value tells our automated test scripts to run your engine on a pc with more memory.
+                //(Unless of course you are already consuming the maximum allowed amount of memory)
+                // If your engine does NOT adhere to this requirement you risk losing points because then our scripts will
+                // mark the test as failed while in reality it just needed a bit more memory
                 std::cerr << "Error: insufficient memory" << std::endl;
                 retVal = 100;
         }
